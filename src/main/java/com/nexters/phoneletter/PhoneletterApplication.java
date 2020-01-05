d5c3d4b51a65995268c09c38cae45351f2a2fe48
정성연 <tjdds1109@naver.com>
2020-01-05 11:47:06 +0900

    first commit

---
+++ src/main/java/com/nexters/phoneletter/PhoneletterApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.phoneletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneletterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PhoneletterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nexters/phoneletter/PhoneletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.phoneletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneletterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
