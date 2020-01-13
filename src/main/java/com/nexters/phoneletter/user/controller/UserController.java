ad985b19edf3f99013196570f71ce2f4687beb2c
정성연 <tjdds1109@naver.com>
2020-01-13 15:10:06 +0900

    스웨거 maven 추가 설정 파일 생성.

--- src/main/java/com/nexters/phoneletter/user/controller/UserController.java
+++ src/main/java/com/nexters/phoneletter/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nexters.phoneletter.user.controller;
 
 import com.nexters.phoneletter.user.repository.UserRepository;
 import com.nexters.phoneletter.user.dto.UserSaveRequestDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,12 @@ public class UserController {
 
   UserRepository userRepository;
 
+  @ApiOperation(value = "유저 회원 가입")
   @PostMapping("/user")
-  public void saveUser(@RequestBody UserSaveRequestDto userSaveRequestDto){
+  public String  saveUser(@RequestBody UserSaveRequestDto userSaveRequestDto){
     userRepository.save(userSaveRequestDto.toEntity());
+    return "success!";
   }
+
+
 }
