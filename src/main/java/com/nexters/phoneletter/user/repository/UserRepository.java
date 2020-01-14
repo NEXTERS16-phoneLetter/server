7a0bd08b7ef87e2d495c57fe70cd38add4766303
hyerin6 <hyerinn6@gmail.com>
2020-01-14 18:00:25 +0900

    jenkins test를 위한 코드 변경

--- src/main/java/com/nexters/phoneletter/user/repository/UserRepository.java
+++ src/main/java/com/nexters/phoneletter/user/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.nexters.phoneletter.user.repository;
 
-import com.nexters.phoneletter.user.domain.Users;
+import com.nexters.phoneletter.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
