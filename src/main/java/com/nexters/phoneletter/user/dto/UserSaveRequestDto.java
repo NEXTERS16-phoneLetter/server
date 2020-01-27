216961c8623611a566d4c5b0017b57a92b942802
정성연 <tjdds1109@naver.com>
2020-01-27 13:39:43 +0900

    UserSaveRequestDto Class @Setter 삭제
    @Controller의 @RequestBody로 Dto 값을 채울때 ,@setter 와 기본 생성자로 값을 받는다는 글을 보고 사용했지만 @setter가 없어도 잘 작동됩니다.

--- src/main/java/com/nexters/phoneletter/user/dto/UserSaveRequestDto.java
+++ src/main/java/com/nexters/phoneletter/user/dto/UserSaveRequestDto.java
@@ -3,10 +3,8 @@ package com.nexters.phoneletter.user.dto;
 import com.nexters.phoneletter.user.domain.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class UserSaveRequestDto {
 
