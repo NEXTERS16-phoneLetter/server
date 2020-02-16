f0b9e7c0284319f5b69a51ee65935d5d4be6d908
hyerin6 <hyerinn6@gmail.com>
2020-02-16 21:55:20 +0900

    Add SMS verify procedure

--- src/main/java/com/nexters/phoneletter/user/service/UserAuthService.java
+++ src/main/java/com/nexters/phoneletter/user/service/UserAuthService.java
@@ -5,6 +5,12 @@ import com.nexters.phoneletter.user.dto.UserAuthDto;
 import javax.servlet.http.HttpServletRequest;
 
 public interface UserAuthService {
+    // 인증 코드 문자 발송
     public void sendSms(UserAuthDto userAuthDto);
+
+    // 사용자가 입력한 인증 코드 확인
     public boolean isMatchVerifyCode(UserAuthDto userAuthDto, HttpServletRequest request);
+
+    // 전화번호 인증 여부 확인
+    public boolean isVerifiedPhoneNumber(String phoneNumber, HttpServletRequest request);
 }
